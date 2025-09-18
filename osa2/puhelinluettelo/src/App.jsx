5ffd8605a0cede78262fe8e7d8cec1e4ec3ae760
aalokor <aalo.korkeaaho@gmail.com>
2025-09-18 18:49:26 +0300

    modify osa2/puhelinluettelo

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 import Filter from './components/Filter'
+import Notification from './components/Notification'
 import personService from './services/persons'
 
 const App = () => {
@@ -9,6 +10,8 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [personfilter, setFilter] = useState('')
+  const [message, setMessage] = useState(null)
+  const [messageType, setMessageType] = useState('')
 
   useEffect(() => {
     personService.getAll().then((initialPersons) => {
@@ -35,11 +38,24 @@ const App = () => {
         personService
           .update(existingPerson.id, personObject)
           .then(response => {
-            setPersons(persons.map(person => person.id !== existingPerson.id ? person : response
-          ))
-          setNewName('')
-          setNewNumber('')
-        })
+            setPersons(persons.map(person => person.id !== existingPerson.id ? person : response))
+            setNewName('')
+            setNewNumber('')
+            setMessage(`Updated ${personObject.name}`)
+            setMessageType('success')
+            setTimeout(() => {
+              setMessage(null)
+              setMessageType('')
+            }, 5000)
+          })
+          .catch(() => {
+            setMessage(`Failed to update ${personObject.name}`)
+            setMessageType('error')
+            setTimeout(() => {
+              setMessage(null)
+              setMessageType('')
+            }, 5000)
+          })
       }
     } else {
     personService
@@ -48,6 +64,20 @@ const App = () => {
         setPersons(persons.concat(returnedPerson))
         setNewName('')
         setNewNumber('')
+        setMessage(`Added ${personObject.name}`)
+        setMessageType('success')
+        setTimeout(() => {
+          setMessage(null)
+          setMessageType('')
+        }, 5000)
+      })
+      .catch(() => {
+        setMessage(`Failed to add ${personObject.name}`)
+        setMessageType('error')
+        setTimeout(() => {
+          setMessage(null)
+          setMessageType('')
+        }, 5000)
       })
     }
   }
@@ -69,6 +99,21 @@ const App = () => {
     personService.delete_person(person.id)
       .then(() => {
         setPersons(persons.filter(p => p.id !== person.id))
+        setMessage(`Deleted ${person.name}`)
+        setMessageType('success_delete')
+        setTimeout(() => {
+          setMessage(null)
+          setMessageType('')
+        }, 5000)
+      })
+      .catch(() => {
+        setMessage(`Information of ${person.name} has already been removed from server`)
+        setMessageType('error')
+        setTimeout(() => {
+          setMessage(null)
+          setMessageType('')
+        }, 5000)
+        setPersons(persons.filter(p => p.id !== person.id))
       })
     }
   }
@@ -76,6 +121,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={message} type={messageType} />
       <Filter personfilter={personfilter} handleFilterChange={handleFilterChange}/>
       <h3>Add a new</h3>
         <PersonForm 
