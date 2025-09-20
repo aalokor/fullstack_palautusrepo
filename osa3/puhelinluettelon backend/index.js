1f8a67589a9ebef7e574d4f7b0716741426c26e8
aalokor <aalo.korkeaaho@gmail.com>
2025-09-20 16:55:40 +0300

    modify puhelinluettelo backend and puhelinluettelo

--- osa3/puhelinluettelon backend/index.js
+++ osa3/puhelinluettelon backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
@@ -24,6 +25,8 @@ const morganLogger = (tokens, req, res) => {
 }
 
 app.use(morgan(morganLogger))
+
+app.use(cors())
 
 let persons = [
     { 
@@ -129,7 +132,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
