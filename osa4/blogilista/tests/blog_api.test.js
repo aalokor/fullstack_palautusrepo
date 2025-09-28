b23c60663b26c4c9afbff1ee469fd1e1249abba7
aalokor <aalo.korkeaaho@gmail.com>
2025-09-28 22:46:58 +0300

    add tests to osa4/blogilista

--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -64,7 +64,85 @@ test('blogs are returned as json', async () => {
 test('all blogs are returned', async () => {
   const response = await api.get('/api/blogs')
   
-  assert.strictEqual(response.body.length, 6)
+  assert.strictEqual(response.body.length, initialBlogs.length)
+})
+
+test('blog identification field is named id', async () => {
+  const response = await api.get('/api/blogs')
+
+  response.body.forEach(blog => {
+    assert.ok(blog.id !== undefined)
+    assert.strictEqual(blog._id, undefined)
+  })
+})
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'Testiblogi',
+    author: 'Teppo Testaaja',
+    url: 'http://testiblogi.com',
+    likes: 10
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+
+  const titles = response.body.map(r => r.title)
+
+  assert.strictEqual(response.body.length, initialBlogs.length + 1)
+
+  assert(titles.includes('Testiblogi'))
+})
+
+test('likes field default value is zero', async () => {
+  const newBlog = {
+    title: 'Testiblogi',
+    author: 'Teppo Testaaja',
+    url: 'http://testiblogi.com'
+  }
+
+  const response = await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  assert.strictEqual(response.body.likes, 0)
+})
+
+test('blog without title is not added and leads to request error', async () => {
+  const newBlog = {
+    title: '',
+    author: 'Teppo Testaaja',
+    url: 'http://testiblogi.com',
+    likes: 10
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('blog without url is not added and leads to request error', async () => {
+  const newBlog = {
+    title: 'Testiblogi',
+    author: 'Teppo Testaaja',
+    url: '',
+    likes: 10
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+    .expect('Content-Type', /application\/json/)
 })
 
 after(async () => {
