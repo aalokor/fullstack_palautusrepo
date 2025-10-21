b01554f895e8adb2ddea6c0755aca161bb093cf1
ellaverak <ella.korkeaaho@gmail.com>
2025-10-21 20:34:24 +0300

    stuff

--- osa5/bloglist-frontend/src/App.jsx
+++ osa5/bloglist-frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import Login from './components/Login'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
@@ -8,11 +10,18 @@ const App = () => {
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
-    )  
+    )
   }, [])
+
+  const handleLogin = event => {
+    event.preventDefault()
+    console.log('logging in with', username, password)
+  }
 
   return (
     <div>
+      <h2>log in to application</h2>
+      <Login handleLogin={handleLogin} />
       <h2>blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
---
+++ osa5/bloglist-frontend/src/components/Login.jsx
@@ -0,0 +1,29 @@
+const Login = ({ handleLogin }) => (
+    <div>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label>
+            username
+            <input
+              type="text"
+              value={username}
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            password
+            <input
+              type="password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </label>
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+)
+
+export default Login
